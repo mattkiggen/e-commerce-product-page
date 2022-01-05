fa4a9a77c307a082c2289a19937c20407bad8fa0
Matthew Kiggen <28730291+mattkiggen@users.noreply.github.com>
2022-01-05 17:17:35 +0200

    add product layout

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,18 +3,29 @@ import styles from '../styles/Product.module.scss';
 export default function Product() {
   return (
     <main className={styles.product}>
-      <img
-        src='/images/image-product-1.jpg'
-        alt='Sneaker'
-        className={styles.featuredImage}
-      />
-      <h1>Fall Limited Edition Sneakers</h1>
-      <p>
-        These low profile sneakers are your perfect casual wear companion.
-        Featuring a durable rubber outer sole, they'll withstand everything the
-        weather can offer.
-      </p>
-      <p>$125.00</p>
+      <div className={styles.details}>
+        <img
+          src='/images/image-product-1.jpg'
+          alt='Sneaker'
+          className={styles.featuredImage}
+        />
+        <div className={styles.content}>
+          <p className={styles.brand}>Sneaker Company</p>
+          <h1>Fall Limited Edition Sneakers</h1>
+          <p className={styles.description}>
+            These low profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they'll withstand everything
+            the weather can offer.
+          </p>
+          <div className={styles.priceInfo}>
+            <div>
+              <p className={styles.price}>$125.00</p>
+              <p className={styles.discount}>50%</p>
+            </div>
+            <p className={styles.originalPrice}>$250.00</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
